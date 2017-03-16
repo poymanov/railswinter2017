e0a7695d51b1253396408065a7e2a13a8e978c11
Nikolay <n.poymanov@gmail.com>
2017-03-16 21:14:07 +0300

    Поправлено задание 21-03

--- np2/lesson21/task-21-03.rb
+++ np2/lesson21/task-21-03.rb
@@ -1,21 +1,12 @@
 # encoding: utf-8
 
 (1..100).each do |number|
-  original_number = number
-
-  # Преобразование в массив
-  number = number.to_s.split("")
-
-  # Преобразование элементов массива в число
-  number.map! { |i| i.to_i}
-
-  # Сумма цифр числа
-  sum = number.inject { |sum, x| sum + x }
 
   # Проверка делимостей
-  div3 = sum % 3 == 0
-  div5 = number.last == 0 || number.last == 5
+  div3 = number % 3 == 0
+  div5 = number % 5 == 0
 
+  # Результат выбора
   if div3 && div5
     result = "тыры-пыры"
   elsif div3
@@ -23,7 +14,7 @@
   elsif div5
     result = "пыры"
   else
-    result = original_number
+    result = number
   end
 
   puts result
