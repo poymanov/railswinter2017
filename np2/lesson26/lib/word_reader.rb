03528649869d06c8dadf571c796065a3d89999dc
Nikolay <n.poymanov@gmail.com>
2017-03-19 12:43:50 +0300

    Добавлено задание 26-1

---
+++ np2/lesson26/lib/word_reader.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+#
+# Класс WordReader, отвечающий за чтение слова для игры.
+class WordReader
+  def read_from_args
+    ARGV[0]
+  end
+
+  def read_from_file(file_name)
+    return nil unless File.exist?(file_name)
+
+    lines = File.readlines(file_name, encoding: 'UTF-8')
+
+    lines.sample.chomp
+  end
+end
